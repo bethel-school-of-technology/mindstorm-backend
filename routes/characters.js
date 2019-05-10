3b7030c38a88583b0015d1fed44f3ca8f9839c32
msiksnis <msiksnis@gmail.com>
2019-05-10 16:28:28 +0200

    Added PUT and GET methods

--- routes/characters.js
+++ routes/characters.js
@@ -1,28 +1,51 @@
-const express = require('express');
-const Character = require('../models/character');
+const express = require("express");
+const Character = require("../models/character");
 
 const router = express.Router();
 
+// PUT a charatcter
+router.put("/:id", (req, res, next) => {
+  const character = new Character({
+    _id: req.body.id,
+    title: req.body.title,
+    detail: req.body.detail
+  });
+  Character.updateOne({ _id: req.params.id }, character).then(result => {
+    res.status(200).json({ message: "Update successful!" });
+  });
+});
+
 // GET list of characters
 router.get("", (req, res, next) => {
-   Character.find().then(docs => {
-       res.status(200).json({
-           characters: docs,
-           message: "Characters United!"
-       });
-   });
+  Character.find().then(docs => {
+    res.status(200).json({
+      characters: docs,
+      message: "Characters United!"
+    });
+  });
+});
+
+// GET character by id
+router.get("/:id", (req, res, next) => {
+  Character.findById(req.params.id).then(character => {
+    if (character) {
+      res.status(200).json(character);
+    } else {
+      res.status(404).json({ message: "Character not found!" });
+    }
+  });
 });
 
 // POST a character
 router.post("", (req, res, next) => {
-    const character = new Character({
-        title: req.body.title,
-        detail: req.body.detail
+  const character = new Character({
+    title: req.body.title,
+    detail: req.body.detail
+  });
+  post.save().then(charCreated => {
+    res.status(201).json({
+      characterId: charCreated._id,
+      message: "Character created!"
     });
-    post.save().then(charCreated => {
-        res.status(201).json({
-            characterId: charCreated._id,
-            message: "Character created!"
-        });
-    });
+  });
 });
