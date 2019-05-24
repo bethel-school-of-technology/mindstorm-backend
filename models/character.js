30af5f1357d7b6dc3eae89b3260001857ebfe8be
msiksnis <msiksnis@gmail.com>
2019-05-24 19:19:05 +0200

    added authorization to character featur

--- models/character.js
+++ models/character.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const characterSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    detail: { type: String, required: false }
+  title: { type: String, required: true },
+  detail: { type: String, required: false },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 module.exports = mongoose.model("Character", characterSchema);
