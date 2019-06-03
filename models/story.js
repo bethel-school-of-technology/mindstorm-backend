581433b7e1d497358ee07e5b48a49a9ce219b39f
Maria <jn-01@centurytel.net>
2019-06-03 09:28:06 -0500

    Added image uploading

--- models/story.js
+++ models/story.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const storySchema = mongoose.Schema({
   storyTitle: { type: String, required: true },
   storyBody: { type: String, required: false },
+  imagePath: { type: String, required: false },
   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
 
